fbf257b1033ecdc33f829f74bbb5db3fc12936ca
Olivier Loukombo <olivierloukombo@gmail.com>
2020-11-26 15:40:17 +0100

    adding a submit route

--- src/Application.kt
+++ src/Application.kt
@@ -8,6 +8,7 @@ import io.ktor.http.*
 import io.ktor.html.*
 import kotlinx.html.*
 import freemarker.cache.*
+import freemarker.core.HTMLOutputFormat
 import io.ktor.freemarker.*
 import io.ktor.http.content.*
 
@@ -16,8 +17,14 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    val blogEntry = mutableListOf(BlogEntry(
+        "The drive to develop",
+        ".. it's what keeps me going."
+    ))
     install(FreeMarker) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+        outputFormat = HTMLOutputFormat.INSTANCE
     }
 
     routing {
@@ -26,27 +33,40 @@ fun Application.module(testing: Boolean = false) {
         }
 
         get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+            call.respond(FreeMarkerContent("index.ftl", mapOf("entries" to blogEntry), ""))
         }
 
-        get("/html-dsl") {
+        post("/submit") {
+            val params = call.receiveParameters()
+            val headline = params["headline"] ?: return@post call.respond(HttpStatusCode.BadRequest)
+            val body = params["body"] ?: return@post call.respond(HttpStatusCode.BadRequest)
+            val newEntry = BlogEntry( headline, body)
+            blogEntry.add(0, newEntry)
+
             call.respondHtml {
+
                 body {
-                    h1 { +"HTML" }
-                    ul {
-                        for (n in 1..10) {
-                            li { +"$n" }
-                        }
+                    h1 {
+                        + "Thanks for submitting your entry !"
+                    }
+
+                    p {
+                        + "We've submitted your entry titled "
+                        b { + newEntry.headline }
+                    }
+
+                    p { + "You have submitted a total of ${blogEntry.count()} articles !" }
+
+                    a("/"){
+                        + "Go back"
                     }
                 }
             }
         }
 
-        get("/html-freemarker") {
-            call.respond(FreeMarkerContent("index.ftl", mapOf("data" to IndexData(listOf(1, 2, 3))), ""))
-        }
     }
 }
 
 data class IndexData(val items: List<Int>)
+data class BlogEntry(val headline: String, val body: String)
 
